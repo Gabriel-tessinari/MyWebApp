08b9c5a4c1719b18f3826ec6da539b6ce7f0d5c5
Gabriel Tessinari <gtcmiranda18@gmail.com>
2020-11-30 15:53:32 -0300

    Update angular.json

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -8,8 +7,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule,
-    RouterModule
-  ]
+    CommonModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UserModule { }
